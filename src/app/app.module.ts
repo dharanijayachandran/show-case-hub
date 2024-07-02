e2d7b71a56d5aec07f3d83a323d01092c6830431
dharanijayachandran <dharanipriya18it@gmail.com>
2024-07-02 18:14:46 +0530

    portf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {MatCardModule} from '@angular/material/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ProjectComponent } from './project/project.component';
+import { ContactComponent } from './contact/contact.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { CopyrightComponent } from './copyright/copyright.component';
+import { AboutComponent } from './about/about.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectComponent,
+    ContactComponent,
+    CopyrightComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule,
+    BrowserAnimationsModule,
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import * as saveAs from 'file-saver';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
+})
+export class ContactComponent implements OnInit{
+  constructor(private FormBuilder:FormBuilder, private httpClient:HttpClient){}
+  contactForm:FormGroup | any;
+ngOnInit(): void {
+  this.contactForm = this.FormBuilder.group({
+    name: [''],
+    email: [''],
+    message: ['']
+  })
+}
+submitForm(){
+console.log("..."+JSON.stringify(this.contactForm.value))
+}
+downloadResume():void{
+ const resumeUrl = 'assets/J_DHARANI_RESUME.pdf'
+ this.httpClient.get(resumeUrl,{responseType:'blob'}).subscribe((blob)=>{
+  saveAs(blob, 'J_Dharani_Resume.pdf');
+ })
+}
+}
