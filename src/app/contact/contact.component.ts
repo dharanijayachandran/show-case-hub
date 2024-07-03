d3b3294cd74073c86552031d6fa37f68c45d2042
dharanijayachandran <dharanipriya18it@gmail.com>
2024-07-03 12:00:37 +0530

    navigation

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import * as saveAs from 'file-saver';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-contact',
@@ -11,20 +12,29 @@ import * as saveAs from 'file-saver';
 export class ContactComponent implements OnInit{
   constructor(private FormBuilder:FormBuilder, private httpClient:HttpClient){}
   contactForm:FormGroup | any;
+  private scriptUrl = '/api'; 
 ngOnInit(): void {
   this.contactForm = this.FormBuilder.group({
     name: [''],
-    email: [''],
+    email: ['',[Validators.required,Validators.email]],
     message: ['']
   })
 }
 submitForm(){
-console.log("..."+JSON.stringify(this.contactForm.value))
+if(this.contactForm.valid){
+  this.contactForm.reset();
+  Swal.fire("Message Sent Successfully!");
+  this.httpClient.post(this.scriptUrl, this.contactForm.value).subscribe((response:any)=>{
+    
+  })
+
 }
+}
+
 downloadResume():void{
  const resumeUrl = './assets/J_DHARANI_RESUME.pdf'
  this.httpClient.get(resumeUrl,{responseType:'blob'}).subscribe((blob)=>{
   saveAs(blob, 'J_Dharani_Resume.pdf');
- })
+ });
 }
 }
