2d3d358e48a86ee822931ccd694d89efbb1fe36d
dharanijayachandran <dharanipriya18it@gmail.com>
2024-07-09 22:39:34 +0530

    animation

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,9 +1,21 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
+  animations: [
+    trigger('tabAnimation', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateX(-50px)' }),
+        animate('300ms ease-out', style({ opacity: 1, transform: 'translateX(0)' })),
+      ]),
+      transition(':leave', [
+        animate('300ms ease-in', style({ opacity: 0, transform: 'translateX(-50px)' })),
+      ]),
+    ]),
+  ],
 })
 export class AboutComponent {
 
